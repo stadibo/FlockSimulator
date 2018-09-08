81717fca2a9acad8f4445ab0fa0a680df14c9d7f
stadibo <jesperleonard@gmail.com>
2018-09-08 22:21:31 +0300

    Project and basic classes initialized
    skeleton structure for project created

---
+++ FlockSimulator/src/main/java/flockSimulator/gui/simulatorUi.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flockSimulator.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author peje
+ */
+public class simulatorUi extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("Hello simulation!");
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
